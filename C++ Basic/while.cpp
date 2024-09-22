60eee466dcc5aa1f05f7259362d4f66741d7e260
Arish Ahmad <67474371+arish-ahmad@users.noreply.github.com>
2024-09-22 21:42:47 +0530

    Add files via upload

---
+++ C++ Basic/while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int counter = 1;
+    
+    while(counter <= 50){
+        
+        cout<<"while loop tutorial"<<endl;
+        counter++;
+
+    }
+
+
+    return 0;
+}
