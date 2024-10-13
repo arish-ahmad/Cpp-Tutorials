5814fb2b2b74528d91353ba727cb151dc47082ef
Arish Ahmad <67474371+arish-ahmad@users.noreply.github.com>
2024-10-13 21:55:57 +0530

    Add files via upload

---
+++ C++ Basic/for.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+int main (){
+
+    for(int i=1; i<=10; i++){
+
+        cout<<"For Loop Tutorial"<<endl;
+    }
+
+
+    /*
+    Infinite loop 
+    
+    for( ; ; ){
+        cout<< "infinite ";
+    }
+    
+    */
+    return 0;
+
+}
+
+
+
