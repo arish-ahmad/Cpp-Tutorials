21c9e02df1dce830765fb561bb6c7ef4e6e8bdb7
Arish Ahmad <67474371+arish-ahmad@users.noreply.github.com>
2024-11-03 23:21:16 +0530

    Add files via upload

---
+++ C++ Basic/arrays.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  //int marks[6] = {56,78,67,54,67,90};
+  
+    int marks[6] {56,78,67,54,67,90};
+
+    // marks[0] = 56;
+    // marks[1] = 78;
+    // marks[2] = 97;
+
+    cout << marks[4] ;
+
+    return 0;
+}
